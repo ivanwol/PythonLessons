5f5080738f3fb728332922da72690948223ed994
Ivan <ivanwolnoe@gmail.com>
2022-01-15 12:58:03 +0200

    My tuple commit №1

---
+++ ListsTuplesDictionaries/tuples.py
@@ -0,0 +1,65 @@
+# users = ("Ivan", 14)
+# print(users)
+# print(type(users))
+
+
+# user = "Ivan", 14
+# print(user)
+
+
+# user = ("Ivan", )
+# print(user)
+
+
+# city = ["Kiev", "Odessa", "Madrid", "Rim"]
+# city_tuple = tuple(city)
+# print(city)
+# print(city_tuple)
+
+
+# users = ("Viktor", "Alex", "Serhii", "Ana")
+# print(users[0])
+# print(users[0:3])
+
+
+# user = ("Ivan", 14, False)
+# name, age, is_married = user
+# print(name)
+# print(age)
+# print(is_married)
+
+
+# def get_user():
+#     name = "Ivan"
+#     age = 14
+#     is_married = False
+#     return name, age, is_married
+#
+#
+# user = get_user()
+# print(user)
+# print(user[0])
+
+
+# korteg = (1, 2.5, "Hi", [], False, ("Hello", ))
+# print(len(korteg))
+
+
+# korteg = (1, 2.5, "Hi", [], False, ("Hello", ))
+# for i in korteg:
+#     print(i)
+
+
+# korteg = (1, 2.5, "Hi", [], False, ("Hello", ))
+# i = 0
+# while i < len(korteg):
+#     print(korteg[i])
+#     i = i + 1
+
+
+# korteg = (1, 2.5, "Hi", [], False, ("Hello", ))
+# question = int(input("Введите значение, которые Вы хотите найти в нашем tuple: "))
+# if question in korteg:
+#     print("Элемент найден")
+# else:
+#     print("Такого элемента здесь нет")
