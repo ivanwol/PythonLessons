e8522f9aff563bd833b4bb9d099f9e86edb71aa0
Ivan <ivanwolnoe@gmail.com>
2022-02-16 20:56:04 +0200

    My string commit №1

---
+++ String/lesson1.py
@@ -0,0 +1,54 @@
+# info = "Привет\n я из Киева"  # Переход на следуйщую строку
+# print(info)
+
+# info = "Привет\t я из Киева"  # Делает табуляцию
+# print(info)
+
+# path = r"C:\python\name.txt"  # Убирает все действия команд для строк
+# print(path)
+
+# name = input("Введите свое имя: ")
+# age = input("Введите свой возраст: ")
+# user = f"name: {name} Мне {age} лет"  # F-string
+# print(user)
+
+# string = "Hello world"
+# print(string[5])
+# s1 = string[2]
+# print(s1)
+# print(string[12])
+
+# string = "Hello world"
+# string[0] = "r"
+
+# string = "Hello world"
+# for i in string:
+#     print(i)
+
+# string = "Hello world"
+# print(string[:5])
+# print(string[2:5])
+
+# print("Hi" * 3)
+
+# print(ord("а"))  # Возвращает значение в таблице unicode
+
+# string = "Hello world"
+# a = "hello" in string
+# print(a)
+# a = "hello"
+# if a in string:
+#     print(True)
+# else:
+#     print(False)
+
+# string = input("Введите строку: ")
+#
+#
+# def strfunct(x):
+#     a = "Hello" in x
+#     return a
+#
+#
+# print(strfunct(string))
+
