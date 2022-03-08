506b4cb2518bcce04559ebaf52af9cd10d0f936a
Ivan <ivanwolnoe@gmail.com>
2022-03-08 18:26:11 +0200

    My string commit №2

---
+++ String/lesson2.py
@@ -0,0 +1,47 @@
+# string = input("Введите число: ")
+# if string.isnumeric(): # Возвращает True если строка написана цифрами
+#     a = int(string)
+#     print(a)
+#     print(type(a))
+# else:
+#     print(string)
+#     print(type(string))
+
+# file_name = "task.txt"
+# start = file_name.startswith("lesson") |
+#                                         Проверка строки на наличие куска с конца или с начала строки
+# end = file_name.endswith(".py")        |
+# print(end)
+
+# string = "   hello  world!  "
+# finall = string.strip()  # Убирает пробелы с краев строки
+# print(string)
+# print(finall)
+
+# print("Madrid", "5000000".rjust(10))  # Делает 10 мини-пробелов между двумя элементами
+
+# welcome = "Hello world! Goodbye world!"
+# index1 = welcome.find("wor")  # Поиск заданной части в строке и принт ее индекса
+# print(index1)
+
+# index2 = welcome.find("wor", 10)  # Поиск заданной части в строке после заданного индекса
+# print(index2)
+#
+# print(welcome[3:20])
+# index3 = welcome.find("wor", 3, 20)  # Поиск заданной части в строке в определенном промежутке
+# print(index3)
+
+# phone = "+1-234-567-89-10"
+# edited_phone = phone.replace("-", "")  # Замена символов в строке на другие символы
+# print(phone)
+# print(edited_phone)
+
+# edited_phone = phone.replace("-", "", 2)  # Замена символов в строке на другие символы некоторое количество раз
+# print(phone)
+# print(edited_phone)
+
+# phone = "380-98-96!40 695"
+# phone1 = phone.replace("-", "")
+# phone2 = phone1.replace("!", "")
+# phone3 = phone2.replace(" ", "")
+# print(phone3)
