22080ffa26fd2c9d40151a18ac3a71b787b2f86e
Ivan <ivanwolnoe@gmail.com>
2022-04-09 10:16:40 +0300

    OOP -super

---
+++ OOP/lesson4.py
@@ -0,0 +1,27 @@
+# class Person:
+#     def __init__(self, name):
+#         self.__name = name
+#
+#     @property
+#     def name(self):
+#         return self.__name
+#
+#     def display_info(self):
+#         print(f"name: {self.__name}")
+#
+#
+# class Employee(Person):
+#     def __init__(self, name, company):
+#         super().__init__(name)
+#         self.company = company
+#
+#     def display_info(self):
+#         super().display_info()
+#         print(f"company: {self.company}")
+#
+#     def work(self):
+#         print(f"{self.name} works")
+#
+#
+# tom = Employee("Tom", "UBER")
+# tom.display_info()
